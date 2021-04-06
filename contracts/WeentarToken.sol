7914cb99cc65f25f4b5ad1fd20864da2f5a67001
Sebastian Dine <sebastiandine91@gmail.com>
2021-04-06 07:36:54 +0200

    Minting schedule (#2)
    * removed minting function from BEP20 contract
    
    * added minting schedule in the mint function
    
    * setting initial supply from the total tokens value
    
    * removed Safemath from the code
    
    * term adjustment
    
    * added imports and edited constructor
    
    * updated check for the symbol
    
    * added new test file
    
    * deleted redundant file
    
    * fixing term
    
    * added extra functions for testing
    
    * added test cases for minting schedule
    
    * adjustment due to the decision to have our own chain for the final platform. Therefore the current token is only a 'placeholder' without any required minting logic.
    
    Co-authored-by: kailehok <baral.kailash12345@gmail.com>
    Co-authored-by: Sebastian Dine <none>

--- contracts/WeentarToken.sol
+++ contracts/WeentarToken.sol
@@ -1,11 +1,16 @@
 pragma solidity 0.8.1;
 
-import "./BEP20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
 
-contract WeentarToken is BEP20 {
+contract WeentarToken is ERC20, Ownable {
 
-    constructor(uint256 initalSupply) BEP20("Weentar Token", "WTR", 18) {
-        _mint(owner(), initalSupply);
+    constructor( uint256 maxSupply) ERC20("Weentar Token", "$WNTR") {
+        _mint(owner(), maxSupply);
+    }
+
+    function getOwner() public view returns (address){
+        return owner();
     }
 
 }
